5b88d36139e0d01bbf73a5ed5a258d7013ed09ad
ReboreExplore <manpabarmanaec@gmail.com>
2021-10-29 21:17:28 +0530

    added some more activation function

--- activation_functions.py
+++ activation_functions.py
@@ -1,6 +1,5 @@
 import math
 
-
 def linear(x):
     '''Linear activation function: https://en.wikipedia.org/wiki/Identity_function
     '''
@@ -80,3 +79,30 @@ def mish(x):
 def bent_identity(x):
     '''Bent-Identity Activation Function: https://www.gabormelli.com/RKB/Bent_Identity_Activation_Function'''
     return (math.sqrt(((x**2)+1)-1)/2)+x
+
+
+def gelu(x):
+    '''GELU Activation Function : https://paperswithcode.com/method/gelu'''
+    cdf = 0.5 * (1.0 + math.erf(x / math.sqrt(2.0)))
+    return x * cdf
+
+def arctan(x):
+    '''Arc Tan Activation Function - http://theurbanengine.com/blog//arctan'''
+    return math.atan(x)
+
+def lecuns_tanh(x):
+    '''Le Cun's tanh ACtivation Function - http://yann.lecun.com/exdb/publis/pdf/lecun-98b.pdf'''
+    return 1.7159 * math.tanh(2/3 * x)
+
+
+def bipolar_sigmoid(x):
+    '''Bipolar Sigmoid - https://aip.scitation.org/doi/pdf/10.1063/1.4954526#:~:text=Bipolar%20sigmoid%20is%20a%20type,it%20is%20a%20bounded%20function.'''
+    return (1 - math.exp(-x)) / (1 + math.exp(-x))
+
+
+def logit(x):
+    '''https://deepai.org/machine-learning-glossary-and-terms/logit#:~:text=The%20Logit%20function%20is%20used,between%20negative%20and%20positive%20infinity.'''
+    try:
+        return math.log(x / (1 - x))
+    except ValueError:
+        print('ignoring math domain error..')
